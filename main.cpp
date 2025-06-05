0de324fcb1d546a328d981a6179b7979d9bf97b1
a.toshbotirov <a.toshbotirov@newuu.uz>
2025-06-05 17:12:30 +0500

    https://github.com/akmal1216/summer-problem2.git

---
+++ main.cpp
@@ -0,0 +1,103 @@
+//problem 1
+
+
+
+// using namespace std;
+// int main() {
+//     int variable;
+//     int variable2;
+//     cin >> variable>>variable2;
+//     int sum = variable + variable2;
+//     cout << sum << endl;
+//     return 0;
+// }
+
+// problem 2
+//  # include <iostream>
+// using namespace std;
+// int main() {
+//  float x;
+//  float y;
+//  cin >>x>>y;
+//  float t=(3*(x*x+3))/((y/4)+3);
+//  cout<<t<<endl;
+//  return 0;
+//
+// }
+
+//problem 3
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int a;
+//     cin >> a;
+//     int b;
+//     cin >> b;
+//     cout<< "Is a < b ?" << (a < b) << endl;
+//     cout<< "Is a > b ?" << (a > b) << endl;
+//     cout<< "Is a = b ?" << (a == b) << endl;
+//     return 0;
+// }
+
+//problem 4
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     float v;
+//     cin >> v;
+//     float y;
+//     cin >> y;
+//     float t;
+//     cin >> t;
+//     float a=(y-v)/t;
+//     cout << a << endl;
+//
+//     return 0;
+//
+// }
+
+//problem 5
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int number;
+//     cin >> number;
+//     number--;
+//     number=(number*7-14)/12;
+//     cout << number << endl;
+//     cout<<number;
+//     int remainder=number/3;
+//     cout<<remainder;
+//
+//
+//     return 0;
+// }
+
+//problem 6
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     float d;
+//     cin >> d;
+//     float a = 4*3.14*d/2*d/2;
+//     cout << a << endl;
+//     return 0;
+// }
+
+// problem 7
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,c,d,sum1, sum2;
+    cout<<"Enter 4 numbers: ";
+    cin>>a>>b>>c>>d;
+
+    sum1=a+b;
+    sum2=c+d;
+
+    cout<< "Is first sum1 < sum2"<< (sum1<sum2)<<endl;
+    cout<< "Is first sum1 > sum2" <<(sum1>sum2)<<endl;
+    cout<< "Are the sums equal?" <<(sum1==sum2)<<endl;
+    return 0;
+}
---
+++ problem2.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Akmaljon Toshbotirov on 05/06/25.
+//
